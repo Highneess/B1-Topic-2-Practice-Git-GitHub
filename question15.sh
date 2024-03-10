8eb1a05ad650d0cbbe1259171c086d3070a6cf08
Highneess <162875634+Highneess@users.noreply.github.com>
2024-03-10 21:48:41 +0800

    Update question15.sh

--- question15.sh
+++ question15.sh
@@ -1,7 +1,5 @@
 git clone https://github.com/ian-knight-uofa/git-practice-01.git
 cd git-practice-01
-git fetch origin branch1
+git fetch --all
 git checkout branch1
 git merge main
-mkdir merged_files
-cp <path-to-conflicting-file> merged_files/
