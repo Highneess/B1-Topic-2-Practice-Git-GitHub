e684dd8445a0d9a6a5295af48b7159f4f5f40d54
Highneess <162875634+Highneess@users.noreply.github.com>
2024-03-10 21:18:45 +0800

    Create question18.sh

---
+++ question18.sh
@@ -0,0 +1,8 @@
+git clone https://github.com/ian-knight-uofa/git-practice-04.git
+cd git-practice-04
+git branch -a
+git checkout main
+git merge ready*
+git branch -d ready*
+git checkout update*
+git merge main
