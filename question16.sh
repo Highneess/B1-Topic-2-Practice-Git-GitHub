01d6ebe355a124aab9cb52d9ba480cf937c6947a
Highneess <162875634+Highneess@users.noreply.github.com>
2024-03-10 21:35:53 +0800

    Create question16.sh

---
+++ question16.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/ian-knight-uofa/git-practice-02.git
+cd git-practice-02
+git checkout branch2
+git merge branch3
+git add .
+git commit -m "Merge branch3 with branch2"
+git branch -d branch3
