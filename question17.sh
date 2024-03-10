39e04234136fd6d630e92a124af85cf43ecc13b2
Highneess <162875634+Highneess@users.noreply.github.com>
2024-03-10 21:51:32 +0800

    Update question17.sh

--- question17.sh
+++ question17.sh
@@ -1,15 +1,12 @@
 git clone https://github.com/ian-knight-uofa/git-practice-03.git
 cd git-practice-03
 git checkout -b branch1
-git rm -r dir1/dir2/foo_modified dir1/dir3/newfile2
-git mv dir3/bar dir1
-touch newfile1
-git add .
-git commit -m "Modify files for branch1"
+git rm -r dir1/dir2
+echo "Modified foo in branch1" > dir1/foo
+git rm dir3/bar_copy
+echo "New file 1 in branch1" > newfile1
+git commit -am "Modify files for branch1"
 git checkout -b branch2
-git rm newfile1
-git mv dir1/foo dir1/dir2/foo_modified
-mkdir -p dir3
-touch dir3/newfile2
-git add .
-git commit -m "Modify files for branch2"
+echo "Modified foo_modified in branch2" > dir1/dir2/foo_modified
+echo "New file 2 in branch2" > dir1/dir3/newfile2
+git commit -am "Modify files for branch2"
