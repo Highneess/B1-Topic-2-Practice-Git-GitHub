ad6599de16e478019d1fc6dff12a0dd8bed41f55
Highneess <162875634+Highneess@users.noreply.github.com>
2024-03-10 23:19:56 +0800

    Update question15.sh

--- question15.sh
+++ question15.sh
@@ -1,8 +1,5 @@
-git clone https://github.com/ian-knight-uofa/git-practice-01.git
 cd git-practice-01
-git checkout main
-git merge branch1
-cp file.sh file.sh.merged
-git add file.sh.merged
-git commit -m "Resolve merge conflicts and add merged files"
-cp file.sh.merged /path/to/your/submission
+git fetch origin  
+git checkout origin
+git checkout main  
+git merge branch1    
