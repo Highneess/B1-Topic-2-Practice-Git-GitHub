811e1ce5b663446b51b30b1ba18ba9f18450c5b3
Highneess <162875634+Highneess@users.noreply.github.com>
2024-03-10 21:47:58 +0800

    Update question14.sh

--- question14.sh
+++ question14.sh
@@ -1,6 +1,4 @@
 git clone https://github.com/ian-knight-uofa/git-practice-04.git
 cd git-practice-04
-git fetch origin update1
+git fetch --all
 git checkout update1
-echo "git fetch origin update1" > question14.sh
-echo "git checkout update1" >> question14.sh
