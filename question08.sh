414170f35d3581d5e0666e9add0297e330cff77b
Highneess <162875634+Highneess@users.noreply.github.com>
2024-03-10 21:43:19 +0800

    Update question08.sh

--- question08.sh
+++ question08.sh
@@ -2,3 +2,7 @@ mkdir dir2
 mv *.txt dir2/
 git add dir2/*.txt
 git commit -m "Move text files to dir2"
+echo "mkdir dir2" > question08.sh
+echo "mv *.txt dir2/" >> question08.sh
+echo "git add dir2/*.txt" >> question08.sh
+echo 'git commit -m "Move text files to dir2"' >> question08.sh
