8c61edb7bd885ba2feedc0bf0ac1f1358c46ec23
Highneess <162875634+Highneess@users.noreply.github.com>
2024-03-10 21:44:39 +0800

    Update question11.sh

--- question11.sh
+++ question11.sh
@@ -2,5 +2,7 @@ git checkout -b branch2
 echo "This is file4" > file4
 git add file4
 git commit -m "Add file4"
-echo "This is a modified content of file4" > file4
+echo "Modified content" >> file4
+git stash
 git checkout main
+git stash apply
