b0c2950a0cbf0337215bee414f3166d7ff878512
Highneess <162875634+Highneess@users.noreply.github.com>
2024-03-10 21:49:44 +0800

    Update question16.sh

--- question16.sh
+++ question16.sh
@@ -1,7 +1,7 @@
 git clone https://github.com/ian-knight-uofa/git-practice-02.git
 cd git-practice-02
+git fetch --all
 git checkout branch2
 git merge branch3
-git add .
-git commit -m "Merge branch3 with branch2"
+git commit -am "Merge branch3 with branch2"
 git branch -d branch3
