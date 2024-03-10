7db3fa255c53efad8d5140bc61ce33a928299f9f
Highneess <162875634+Highneess@users.noreply.github.com>
2024-03-10 23:09:07 +0800

    Update question14.sh

--- question14.sh
+++ question14.sh
@@ -1,5 +1,4 @@
 git clone https://github.com/ian-knight-uofa/git-practice-04.git
 cd git-practice-04
+git fetch origin
 git checkout update1
-echo 'git checkout update1' > question14.sh
-chmod +x question14.sh
