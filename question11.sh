db818a4afd6ed53f52cc28f6ef3714edd1e903e3
Highneess <162875634+Highneess@users.noreply.github.com>
2024-03-10 22:39:29 +0800

    Update question11.sh

--- question11.sh
+++ question11.sh
@@ -1,7 +1,8 @@
 git checkout -b branch2
-touch file4  
-git add file4 
+touch file4
+git add file4
 git commit -m "file4"
-git stash  
-git checkout main 
-git stash apply   
+git stash save "Work in progress"
+git checkout main
+git stash apply  
+git stash drop
