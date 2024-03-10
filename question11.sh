936c34c349fc5cb5fe99d9c199d24826745ed679
Highneess <162875634+Highneess@users.noreply.github.com>
2024-03-10 21:25:30 +0800

    Update question11.sh

--- question11.sh
+++ question11.sh
@@ -1,6 +1,6 @@
 git checkout -b branch2
-touch file4  
-git add file4 
-git commit -m "file4"
-echo "Modify file4" >> file4 
-git checkout main 
+echo "This is file4" > file4
+git add file4
+git commit -m "Add file4"
+echo "This is a modified content of file4" > file4
+git checkout main
