414fd163f37f47775ff6cf59a06e413463355756
Highneess <162875634+Highneess@users.noreply.github.com>
2024-03-10 21:33:54 +0800

    Create question14.sh

---
+++ question14.sh
@@ -0,0 +1,6 @@
+git clone https://github.com/ian-knight-uofa/git-practice-04.git
+cd git-practice-04
+git fetch origin update1
+git checkout update1
+echo "git fetch origin update1" > question14.sh
+echo "git checkout update1" >> question14.sh
