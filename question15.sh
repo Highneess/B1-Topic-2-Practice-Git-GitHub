ab560043249e68122bc1ddb755763dbc172d2845
Highneess <162875634+Highneess@users.noreply.github.com>
2024-03-10 21:34:56 +0800

    Create question15.sh

---
+++ question15.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/ian-knight-uofa/git-practice-01.git
+cd git-practice-01
+git fetch origin branch1
+git checkout branch1
+git merge main
+mkdir merged_files
+cp <path-to-conflicting-file> merged_files/
